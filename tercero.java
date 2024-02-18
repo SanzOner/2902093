e333d1091a0e10d1b75ef510d678d353fa435f3e
SanzOner <159849077+SanzOner@users.noreply.github.com>
2024-02-18 16:42:44 -0500

    I finally made the commit

---
+++ tercero.java
@@ -0,0 +1,8 @@
+public class Recordchanges {
+
+    public static void main (String[] args) {
+    Systmen.out.println("First change.");
+    Systmen.out.println("Second change.");
+    Systmen.out.println("Third change.");
+    }
+}
